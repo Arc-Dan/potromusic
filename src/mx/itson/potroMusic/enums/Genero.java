9436993f706cb76e21416755f619f4e724803896
dag13 <dag13@L-15-cs-3>
2025-03-13 09:55:00 -0800

    Creación de paquete entidades y enum, con sus respectivas clases y enum.

---
+++ src/mx/itson/potroMusic/enums/Genero.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.potroMusic.enums;
+
+/**
+ *
+ * @author dag13
+ */
+public enum Genero {
+    
+    POP,
+    ROCK,
+    DANCE,
+    SALSA,
+    OTRO
+}
